318bb583eba9d507b2bf51b6f5aa955672691112
PC-20190315DLDZ\Administrator <18630840206@163.com>
2020-01-09 16:16:27 +0800

    Vue-DataV-Demo

---
+++ src/components/waterLevelChart.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="water-level-chart">
+    <div class="water-level-chart-title">计划资金累计完成情况</div>
+
+    <div class="water-level-chart-details">
+      累计完成<span>235,680</span>元
+    </div>
+
+    <div class="chart-container">
+	  <dv-decoration-9 style="width:150px;height:150px;">66%</dv-decoration-9>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WaterLevelChart',
+  data () {
+    return {
+      config: {
+        data: [45],
+        shape: 'round',
+        waveHeight: 25,
+        waveNum: 2
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#water-level-chart {
+  width: 20%;
+  box-sizing: border-box;
+  margin-left: 20px;
+  background-color: rgba(6, 30, 93, 0.5);
+  border-top: 2px solid rgba(1, 153, 209, .5);
+  display: flex;
+  flex-direction: column;
+  .water-level-chart-title {
+    font-weight: bold;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    justify-content: center;
+  }
+  .water-level-chart-details {
+    height: 12%;
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+    align-items: flex-end;
+    span {
+      font-size: 27px;
+      font-weight: bold;
+      color: #58a1ff;
+      margin: 0 5px;
+      margin-bottom: -5px;
+    }
+  }
+  .chart-container {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="home">
+		<dv-full-screen-container>
+			<top-header />
+			<div class="main-content">
+				<digital-flop />
+				<div class="block-left-right-content">
+					<ranking-board />
+					<div class="block-top-bottom-content">
+						<div class="block-top-content">
+							<rose-chart />
+
+							<water-level-chart />
+
+							<scroll-board />
+						</div>
+						<div class="block-bottom-content">
+							
+						 <cards />
+						</div>
+					</div>
+				</div>
+			</div>
+		</dv-full-screen-container>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import topHeader from '../components/topHeader.vue';
+import digitalFlop from '../components/digitalFlop.vue';
+import rankingBoard from '../components/rankingBoard.vue';
+import roseChart from '../components/roseChart.vue';
+import waterLevelChart from '../components/waterLevelChart.vue';
+import scrollBoard from '../components/scrollBoard.vue';
+import cards from '../components/cards.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'home',
+	components: {
+		topHeader,
+		digitalFlop,
+		rankingBoard,
+		roseChart,
+		waterLevelChart,
+		scrollBoard,
+		cards
+	}
+};
 </script>
+<style lang="scss">
+#home {
+	width: 100%;
+	height: 100%;
+	background-color: #030409;
+	color: #fff;
+	#dv-full-screen-container {
+		width: 100%;
+		height: 100%;
+		background-image: url('../assets/img/bg.png');
+		background-size: 100% 100%;
+		box-shadow: 0 0 3px blue;
+		display: flex;
+		flex-direction: column;
+	}
+	.main-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+	}
+	.block-left-right-content {
+		flex: 1;
+		display: flex;
+		margin-top: 20px;
+	}
+	.block-top-bottom-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		padding-left: 20px;
+	}
+	.block-top-content {
+		height: 55%;
+		display: flex;
+		flex-grow: 0;
+		box-sizing: border-box;
+		padding-bottom: 20px;
+	}
+	.block-bottom-content {
+		height: 45%;
+		display: flex;
+		flex-grow: 0;
+		box-sizing: border-box;
+	}
+}
+</style>
